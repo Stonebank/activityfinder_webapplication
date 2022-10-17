abdd3a6e9d3160c4f80ccb69240217f03c59c09a
Hassan K <hassan_99@live.dk>
2022-10-17 09:31:57 +0200

    Added lombok support for contact model

---
+++ src/main/java/com/hk/activityfinder/model/User.java
@@ -0,0 +1,16 @@
+package com.hk.activityfinder.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class User {
+
+    private String name;
+    private String email;
+
+    private String topic;
+    private String message;
+
+
+}
