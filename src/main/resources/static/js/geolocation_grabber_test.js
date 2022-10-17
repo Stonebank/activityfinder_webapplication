fbb7e8b5b8005ea2e044f13c18a978abc2a73ab4
Hassan K <hassan_99@live.dk>
2022-10-17 09:48:34 +0200

    Removed unnecessary js files

--- src/main/resources/static/js/geolocation_grabber_test.js
+++
@@ -1,6 +0,0 @@
-navigator.geolocation.getCurrentPosition(pos => {
-    const lat = pos.coords.latitude;
-    const lon = pos.coords.longitude;
-    const accuracy = pos.coords.accuracy;
-    console.log(lat + " " + lon + " " + accuracy);
-})
--- src/main/resources/static/js/landingpage.js
+++
@@ -1,6 +0,0 @@
-/*
-* * TODO:
-* * 1. Retyping title
-* * 2. Image transition
-* *
- */
