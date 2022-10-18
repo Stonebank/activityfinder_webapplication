42028d1196154176f90533e1c7b022275f2bdce6
Hassan K <hassan_99@live.dk>
2022-10-18 08:12:40 +0200

    Added UserLocation.java to store and calculate distances between activity and user

---
+++ src/main/java/com/hk/activityfinder/model/location/UserLocation.java
@@ -0,0 +1,12 @@
+package com.hk.activityfinder.model.location;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class UserLocation {
+
+    private double latitude;
+    private double longitude;
+
+}
