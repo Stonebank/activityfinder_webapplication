4f6a7899ef6d48c6a0f430d7421ba3422a48d115
Hassan K <hassan_99@live.dk>
2022-10-18 09:01:45 +0200

    Added more data fields

--- src/main/java/com/hk/activityfinder/model/activities/Activity.java
+++ src/main/java/com/hk/activityfinder/model/activities/Activity.java
@@ -1,14 +1,39 @@
 package com.hk.activityfinder.model.activities;
 
+import com.hk.activityfinder.model.location.coordinate.Coordinate;
+import com.hk.activityfinder.model.location.weather.WeatherType;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 @Getter @Setter
 public class Activity {
 
+    public static final ArrayList<Activity> activities = new ArrayList<>();
+
     private String name;
-    private String address;
+    private String city;
+
+    private int points;
 
     private double distance;
+
+    private Coordinate coordinate;
+
+    private WeatherType[] weatherTypes;
+
+    @Override
+    public String toString() {
+        return "Activity{" +
+                "name='" + name + '\'' +
+                ", address='" + city + '\'' +
+                ", points=" + points +
+                ", distance=" + distance +
+                ", coordinate=" + coordinate +
+                ", weatherTypes=" + Arrays.toString(weatherTypes) +
+                '}';
+    }
 
 }
