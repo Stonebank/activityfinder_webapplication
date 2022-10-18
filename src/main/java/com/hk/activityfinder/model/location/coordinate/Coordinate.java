7ad920c8ecafea720e838222067c0aad2a412b75
Hassan K <hassan_99@live.dk>
2022-10-18 09:02:15 +0200

    Coordinate will store lat and lon

---
+++ src/main/java/com/hk/activityfinder/model/location/coordinate/Coordinate.java
@@ -0,0 +1,28 @@
+package com.hk.activityfinder.model.location.coordinate;
+
+public class Coordinate {
+
+    private final double latitude;
+    private final double longitude;
+
+    public Coordinate(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    @Override
+    public String toString() {
+        return "Coordinate{" +
+                "latitude=" + latitude +
+                ", longitude=" + longitude +
+                '}';
+    }
+}
