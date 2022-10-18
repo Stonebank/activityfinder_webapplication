79b1be1a5c3af8ff134415569903de69bf67fcdc
Hassan K <hassan_99@live.dk>
2022-10-18 19:54:09 +0200

    Constant to hold variables that are final

---
+++ src/main/java/com/hk/settings/Constant.java
@@ -0,0 +1,10 @@
+package com.hk.settings;
+
+public class Constant {
+
+    public static final boolean DEBUG = true;
+
+    public static final String WEATHER_API_KEY = "a0a4ba68146a9c6451e13417ea962641";
+    public static final String WEATHER_UNIT_OUTPUT = "metric";
+
+}
