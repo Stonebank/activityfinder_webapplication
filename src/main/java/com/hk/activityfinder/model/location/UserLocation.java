7ba8b4c81c3efd09035dcf4c89c35c0dad1ddee4
Hassan K <hassan_99@live.dk>
2022-10-18 08:59:03 +0200

    Added coordinate class to handle lat and lon and a method to calculate distance between two coordinate set

--- src/main/java/com/hk/activityfinder/model/location/UserLocation.java
+++ src/main/java/com/hk/activityfinder/model/location/UserLocation.java
@@ -1,12 +1,30 @@
 package com.hk.activityfinder.model.location;
 
+import com.hk.activityfinder.model.location.coordinate.Coordinate;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
 public class UserLocation {
 
-    private double latitude;
-    private double longitude;
+    private Coordinate coordinate;
+
+    public double getDistance(Coordinate coordinate) {
+        int earth_radius = 6371;
+
+        double lat1 = Math.min(this.coordinate.getLatitude(), coordinate.getLatitude());
+        double lat2 = Math.max(this.coordinate.getLatitude(), coordinate.getLatitude());
+
+        double lon1 = Math.min(this.coordinate.getLongitude(), coordinate.getLongitude());
+        double lon2 = Math.max(this.coordinate.getLongitude(), coordinate.getLongitude());
+
+        double deltaLat = Math.toRadians(lat2 - lat1);
+        double deltaLon = Math.toRadians(lon2 - lon1);
+
+        double a = Math.sin(deltaLat / 2) * Math.sin(deltaLat / 2) + Math.cos(lat1) * Math.cos(lat2) * Math.sin(deltaLon / 2) * Math.sin(deltaLon / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        return earth_radius * c;
+    }
 
 }
