aeb9c18ca38fb4b279c0e7d65201bc203ac81b10
Hassan K <hassan_99@live.dk>
2022-10-18 08:15:59 +0200

    Activity data class to store information

---
+++ src/main/java/com/hk/activityfinder/model/activities/Activity.java
@@ -0,0 +1,14 @@
+package com.hk.activityfinder.model.activities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Activity {
+
+    private String name;
+    private String address;
+
+    private double distance;
+
+}
