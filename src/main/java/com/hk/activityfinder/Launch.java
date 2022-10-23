18e2ca11689b79c09421cb03ec7ed524b34528e1
Hassan K <hassan_99@live.dk>
2022-10-23 11:36:52 +0200

    JSONConverter executes upon Launch of backend systems

--- src/main/java/com/hk/activityfinder/Launch.java
+++ src/main/java/com/hk/activityfinder/Launch.java
@@ -1,15 +1,19 @@
 package com.hk.activityfinder;
 
+import com.hk.activityfinder.utility.JSONConverter;
 import com.hk.activityfinder.utility.loader.ActivityLoader;
 import com.hk.activityfinder.utility.loader.MemberLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
 
 @SpringBootApplication
 public class Launch {
 
     public static void main(String[] args) {
         SpringApplication.run(Launch.class, args);
+        new JSONConverter(new File("./activity.txt"), "./activity.json").convert();
         new ActivityLoader().init();
         new MemberLoader().init();
     }
