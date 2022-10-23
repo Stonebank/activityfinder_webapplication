e8a1f4f55eace75e40d41a5da1b1939d829dc333
Hassan K <hassan_99@live.dk>
2022-10-23 05:57:10 +0200

    Added load by email

--- src/main/java/com/hk/activityfinder/interfaces/MemberService.java
+++ src/main/java/com/hk/activityfinder/interfaces/MemberService.java
@@ -2,12 +2,11 @@ package com.hk.activityfinder.interfaces;
 
 import com.hk.activityfinder.dto.Member;
 
-import java.util.List;
-
 public interface MemberService {
 
     void saveUser(Member member);
 
+    Member load(String email);
     Member load(Member member);
 
 }
