302e48c911645c70458394bc9203770f018440dd
Hassan K <hassan_99@live.dk>
2022-10-23 05:38:18 +0200

    Added a static arraylist to cache member data

--- src/main/java/com/hk/activityfinder/dto/Member.java
+++ src/main/java/com/hk/activityfinder/dto/Member.java
@@ -4,8 +4,12 @@ import com.hk.activityfinder.utility.Utils;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+
 @Getter @Setter
 public class Member {
+
+    public static ArrayList<Member> members = new ArrayList<>();
 
     private int id = Utils.generateUniqueID();
 
